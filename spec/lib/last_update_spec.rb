7a34a5a9be44fdf1bb2caf42d047c5b92a8d06d2
Codemis <johnathan@missionaldigerati.com>
2014-05-20 18:12:38 -0700

    Wrapped tests in describe block.

--- spec/lib/last_update_spec.rb
+++ spec/lib/last_update_spec.rb
@@ -9,20 +9,24 @@ RSpec.describe LastUpdate do
     @last_update_file = File.expand_path("../../support/last_update.yml", __FILE__)
   end
 
-  it "should save the last update for a blog to a YAML file" do
-    last_update = LastUpdate.new(@last_update_file)
-    expected_date = DateTime.now
-    last_update.save('blog', expected_date)
-    contents = YAML.load_file(@last_update_file)
-    expect(contents['last_update']['blog']['date']).to eq(expected_date.to_s)
-  end
+  describe "#save" do
 
-  it "should save the last update for a podcast to a YAML file" do
-    last_update = LastUpdate.new(@last_update_file)
-    expected_date = DateTime.now
-    last_update.save('podcast', expected_date)
-    contents = YAML.load_file(@last_update_file)
-    expect(contents['last_update']['podcast']['date']).to eq(expected_date.to_s)
+    it "should save the last update for a blog to a YAML file" do
+      last_update = LastUpdate.new(@last_update_file)
+      expected_date = DateTime.now
+      last_update.save('blog', expected_date)
+      contents = YAML.load_file(@last_update_file)
+      expect(contents['last_update']['blog']['date']).to eq(expected_date.to_s)
+    end
+
+    it "should save the last update for a podcast to a YAML file" do
+      last_update = LastUpdate.new(@last_update_file)
+      expected_date = DateTime.now
+      last_update.save('podcast', expected_date)
+      contents = YAML.load_file(@last_update_file)
+      expect(contents['last_update']['podcast']['date']).to eq(expected_date.to_s)
+    end
+
   end
 
 end
