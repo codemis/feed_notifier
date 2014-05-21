94f9730a30524d06a65ffa52ddc34e71c3da98da
Codemis <johnathan@missionaldigerati.com>
2014-05-20 18:28:32 -0700

    Added validation for all argument variables.

--- lib/last_update.rb
+++ lib/last_update.rb
@@ -11,9 +11,31 @@ class LastUpdate
   # read and save the date time to the configuration file
   #
   def save(key, date_time)
+    raise ArgumentError, "key must be either blog or podcast." unless valid_key?(key)
+    raise ArgumentError, "date_time must be a DateTime." unless valid_date_time?(date_time)
+
     contents = YAML.load_file(@config_file)
     contents['last_update'][key]['date'] = date_time.to_s
     File.open(@config_file, 'w') {|f| f.write contents.to_yaml }
   end
+
+  # Gets the last updated date and returns the date time object
+  #
+  def datetime(key)
+    raise ArgumentError, "key must be either blog or podcast." unless valid_key?(key)
+    contents = YAML.load_file(@config_file)
+    DateTime.parse(contents['last_update'][key]['date'])
+  end
+
+  private
+    def valid_key?(key)
+      ['blog', 'podcast'].include?(key)
+    end
+
+    # Checks if a date is a valid DateTime object
+    #
+    def valid_date_time?(date_time)
+      date_time.is_a?(DateTime)
+    end
 
 end
--- spec/lib/last_update_spec.rb
+++ spec/lib/last_update_spec.rb
@@ -27,6 +27,39 @@ RSpec.describe LastUpdate do
       expect(contents['last_update']['podcast']['date']).to eq(expected_date.to_s)
     end
 
+    it "should throw an error if the key is not blog or podcast" do
+      last_update = LastUpdate.new(@last_update_file)
+      expect { last_update.save('flame', DateTime.now) }.to raise_error(ArgumentError)
+    end
+
+    it "should throw an error if the date_time is not a DateTime" do
+      last_update = LastUpdate.new(@last_update_file)
+      expect { last_update.save('podcast', DateTime.now.to_s) }.to raise_error(ArgumentError)
+    end
+
+  end
+
+  describe "#datetime" do
+
+    it "should return the last update date for a blog post" do
+      contents = YAML.load_file(@last_update_file)
+      expected_date = DateTime.parse(contents['last_update']['blog']['date'])
+      last_update = LastUpdate.new(@last_update_file)
+      expect(last_update.datetime('blog')).to eq(expected_date)
+    end
+
+    it "should return the last update date for a podcast" do
+      contents = YAML.load_file(@last_update_file)
+      expected_date = DateTime.parse(contents['last_update']['podcast']['date'])
+      last_update = LastUpdate.new(@last_update_file)
+      expect(last_update.datetime('podcast')).to eq(expected_date)
+    end
+
+    it "should throw an error if the key is not blog or podcast" do
+      last_update = LastUpdate.new(@last_update_file)
+      expect { last_update.datetime('feeder') }.to raise_error(ArgumentError)
+    end
+
   end
 
 end
