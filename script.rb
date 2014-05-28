2f8568ef97a2e96f1a7023bc773fb8ea2eb40e14
Codemis <johnathan@missionaldigerati.com>
2014-05-27 18:18:39 -0700

    Added all functionality for the script.

--- script.rb
+++ script.rb
@@ -1,3 +1,5 @@
+require 'open-uri'
+
 # require all lib files
 #
 Dir[File.join(Dir.pwd, 'lib', '*.rb')].each { |f| require f }
@@ -5,7 +7,30 @@ Dir[File.join(Dir.pwd, 'lib', '*.rb')].each { |f| require f }
 #
 parse_settings_file = File.expand_path(File.join(Dir.pwd, 'settings', 'parse.yml'), __FILE__)
 parse_settings = YAML.load_file(parse_settings_file)
+parse_notify = ParseNotify.new(parse_settings['parse'])
 
-# parse_notify = ParseNotify.new(parse_settings['parse'])
-# message = 'New Sermon: Love Zombies Too'
-# parse_notify.send(message)
+# Setup the LastUpdate Class
+#
+last_update_file = File.expand_path(File.join(Dir.pwd, 'settings', 'last_update.yml'), __FILE__)
+last_update = LastUpdate.new(last_update_file)
+
+feeds = { 'blog'      =>  'http://www.sgucblog.com/feed/',
+          'podcast'   =>  'http://sgucandcs.org/podcast.php?pageID=38'}
+
+feeds.each do |slug, url|
+  puts "Checking URL: #{url}"
+  # Check if there is a new blog post
+  #
+  last_update_datetime = last_update.find(slug)
+  feed = Feed.new(URI.parse(url))
+
+  # # Check if there is a new post
+  # #
+  if feed.has_new?(last_update_datetime)
+    puts "We have a new #{slug} entry titled #{feed.latest_entry.title}"
+    # Push the new blog entry
+    #
+    parse_notify.send("New #{slug.capitalize} Entry: #{feed.latest_entry.title}")
+    last_update.save(slug, DateTime.now)
+  end
+end
