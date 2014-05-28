20905f789649d5e344170a1765ea94b37c6cd104
Codemis <johnathan@missionaldigerati.com>
2014-05-28 10:44:30 -0700

    Added licensing information.

--- script.rb
+++ script.rb
@@ -1,8 +1,28 @@
+# This file is part of Feed Parse Push Notifier.
+# 
+# Upload Test Files is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# Joshua Project API is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see 
+# <http://www.gnu.org/licenses/>.
+# 
+# @author Johnathan Pulos <johnathan@missionaldigerati.org>
+# @license http://opensource.org/licenses/gpl-license.php GNU Public License
+
 require 'open-uri'
 
 # require all lib files
 #
 Dir[File.join(Dir.pwd, 'lib', '*.rb')].each { |f| require f }
+
 # Setup the Parse Settings
 #
 parse_settings_file = File.expand_path(File.join(Dir.pwd, 'settings', 'parse.yml'), __FILE__)
